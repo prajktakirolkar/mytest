1430bb71333479ccf53535750410747180fb7806
prajktakirolkar <prajktakirolkar16>
2017-03-31 17:54:13 +0530

    change port number

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //bfb96733b03b5837c96c76cfb0556aa0
 
 var router = require("./router.js");
+var port = process.env.PORT || 8000
 
 var http = require('http');
 http.createServer(function(request, response){
@@ -8,9 +9,11 @@ http.createServer(function(request, response){
 	router.homeRoute(request,response);
 	router.userRoute(request,response);
 
-}).listen(8080, '0.0.0.0', function() {
-    console.log('Listening to port:  ' + 3000);
+}).listen(port, '0.0.0.0', function() {
+    console.log('Listening to port:  ' + port);
 });
 
 console.log('Server running at localhost:8080');
+
+
 
