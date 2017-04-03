e69ea7750e0f37a3a85102221162078d049557a6
prajktakirolkar <prajktakirolkar16@gmail.com>
2017-04-03 11:48:38 +0530

    cc

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var router = require("./router.js");
 var port = process.env.PORT || 8080
 
 var http = require('http');
-http.createServer(function(request, response){
+var app = express.createServer(express.logger(),function(request, response){
 
 	router.homeRoute(request,response);
 	router.userRoute(request,response);
@@ -12,6 +12,10 @@ http.createServer(function(request, response){
 }).listen(port, '0.0.0.0', function() {
     console.log('Listening to port:  ' + port);
 });
+
+
+
+//var app = express.createServer(express.logger());
 
 
 
