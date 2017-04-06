b733c16ef608eed97ec9448894b18a1853bf1e2b
prajktakirolkar <prajktakirolkar16@gmail.com>
2017-04-06 12:16:51 +0530

    c

--- weather.js
+++ weather.js
@@ -1,12 +1,10 @@
-var EventEmitter = require("events").EventEmitter;
+
 var http = require("http");
 var util = require("util");
 
 function City(city){
 
-  EventEmitter.call(this);
-  profileEmitter = this;
-
+  
 //Connect to the API
   var request = http.get("http://api.openweathermap.org/data/2.5/weather?q="+ city +"&units=metric&APPID=bfb96733b03b5837c96c76cfb0556aa0", function(response){
   var body = "";
@@ -14,27 +12,27 @@ function City(city){
   if(response.statusCode !==200){
         request.abort();
 
-        profileEmitter.emit("error", new Error("There was an error getting the weather data for " + city + ". (" + http.STATUS_CODES[response.statusCode] + ")"));
+        
    }
 
     response.on('data', function (chunk) {
       	body += chunk;
-      	profileEmitter.emit("data", chunk);
+      	
     });
 
     response.on('end', function(){
 	if(response.statusCode === 200){
    	  try{
               var weatherData = JSON.parse(body);
-              profileEmitter.emit("end", weatherData);
+             
            } 
 	catch(error){
-              profileEmitter.emit("error", error);
+             
            }
         }
     
     }).on("error", function(error){
-      profileEmitter.emit("error", error);
+      
     });
 
   });
